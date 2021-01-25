6fffa61f86cb234ab8dfe3cb71f2095d6997b0d5
Jonathan <jonathan.de.roo@gmail.com>
2021-01-25 22:58:01 +0100

    Powershell log function

---
+++ powershell/log_function.ps1
@@ -0,0 +1,36 @@
+﻿$LogFile = "test.log"
+
+function Initiate-Log {
+    [CmdletBinding()]
+    param(
+        [ValidateNotNullOrEmpty()]
+        [String]$Name
+    )
+
+    If(-Not (Test-Path -Path ".\logs\$Name")){
+        If(-Not (Test-Path -Path '.\logs')){
+            [void](New-Item -Path '.' -Name 'logs' -ItemType 'directory')
+        }
+        [void](New-Item -Path 'logs' -Name $Name -ItemType 'file')
+    }
+}
+
+function Write-Log {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [String]$Message,
+
+        [Parameter()]
+        [ValidateSet('INFO', 'WARN', 'ERROR')]
+        [String]$Severity = 'INFO'
+    )
+
+    $Date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    Add-Content -Path (".\logs\" + $LogFile) -Value "$Date [$Severity] $Message"
+}
+
+Initiate-Log -Name $LogFile
+Write-Log -Severity ERROR -Message "Finished"
